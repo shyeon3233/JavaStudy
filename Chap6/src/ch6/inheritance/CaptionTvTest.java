7b9dd5aefcdf88da74b3423961146c0d9f0cca57
SeokHyeon Jeong <shyeon3233@naver.com>
2022-06-24 22:34:50 +0900

    06/24 java

--- Chap6/src/ch6/inheritance/CaptionTvTest.java
+++ Chap6/src/ch6/inheritance/CaptionTvTest.java
@@ -16,10 +16,25 @@ class Tv{
 }
 
 class CaptionTv extends Tv{
+	boolean caption;
 	
+	void displayCaption(String text) {
+		if(caption) {
+			System.out.println(text);
+		}
+	}
 }
 
 public class CaptionTvTest {
-
-
+	public static void main(String[] args) {
+		CaptionTv ctv = new CaptionTv();
+		ctv.channel = 10;
+		ctv.channelUp();
+		
+		System.out.println(ctv.channel);
+		ctv.displayCaption("Hello World");
+		ctv.caption = true;
+		ctv.displayCaption("Hello World");
+	}
+ 
 }
